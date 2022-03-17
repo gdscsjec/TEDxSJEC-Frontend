be405ee5633e1953a70f095747001cf9e43dd0cc
vigneshshettyin <vigneshshetty.in@gmail.com>
2022-03-17 10:29:22 +0530

    Google Auth Button Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import PaymentButton from "./components/paymentButton";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>This is a tedxsjec react app!!</h1>
+      <PaymentButton />
+    </>
   );
-}
+};
 
 export default App;
