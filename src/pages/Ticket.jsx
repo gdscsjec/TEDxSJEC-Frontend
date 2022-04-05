7e6b17343da99232ce921a51d22ab9dd19354c7d
vigneshshettyin <vigneshshetty.in@gmail.com>
2022-04-05 22:33:13 +0530

    Ticket Download Options!

--- src/pages/Ticket.jsx
+++ src/pages/Ticket.jsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Logo from "../components/Logo";
 import { ParentWrapper, ChildWrapper } from "../styles/layout.style";
+import QRCode from "react-qr-code";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 const Ticket = () => {
   const [ticket, setTicket] = useState();
@@ -20,12 +23,45 @@ const Ticket = () => {
     request();
   }, [params.id]);
 
+  const download = () => {
+    const input = document.getElementById("tedxsjec-ticket");
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "JPEG", 0, 0);
+      pdf.save(`TEDxSJEC-2022-Ticket-${params.id}.pdf`);
+    });
+  };
+
   return (
     <ParentWrapper>
       <Logo />
-      <ChildWrapper id="image">
-        <img src={`https://ted.vigneshcodes.in${ticket.qrcode}`} alt="" />
+      <ChildWrapper id="tedxsjec-ticket">
+        <h1>Ticket</h1>
+        <p>
+          <b>Ticket ID:</b> {params.id}
+        </p>
+        <p>
+          <b>Name:</b> {ticket && ticket.name}
+        </p>
+        <p>
+          <b>Email:</b> {ticket && ticket.email}
+        </p>
+        <p>
+          <b>Phone:</b> {ticket && ticket.phone}
+        </p>
+        <p>
+          <QRCode value={params.id} size={200} />
+        </p>
       </ChildWrapper>
+      <button
+        onClick={() => {
+          download();
+        }}
+        className="btn btn-danger mt-3"
+      >
+        Download Ticket
+      </button>
     </ParentWrapper>
   );
 };
