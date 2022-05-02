12c11cbebbe08de06e5507169486bc4c84e0e3a8
vigneshshettyin <vigneshshetty.in@gmail.com>
2022-05-02 17:30:58 +0530

    Deploy Changes
    @vercel

--- src/pages/Ticket.jsx
+++ src/pages/Ticket.jsx
@@ -2,14 +2,24 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Logo from "../components/Logo";
-import { ParentWrapper, ChildWrapper, QRCodeContainer, TicketSection, TicketHeading, TicketText, OrderIdContainer, OrderId, ErrorText } from "../styles/layout.style";
+import {
+  ParentWrapper,
+  ChildWrapper,
+  QRCodeContainer,
+  TicketSection,
+  TicketHeading,
+  TicketText,
+  OrderIdContainer,
+  OrderId,
+  ErrorText,
+} from "../styles/layout.style";
 import QRCode from "react-qr-code";
 // Optional import
 // import jsPDF from "jspdf";
-import Lottie from 'react-lottie';
-import notFoundAnimation from '../lotties/not-found.json';
+import Lottie from "react-lottie";
+import notFoundAnimation from "../lotties/not-found.json";
 import LoadingOverlay from "react-loading-overlay";
-import { toPng } from 'dom-to-image';
+import { toPng } from "dom-to-image";
 import { useQuery } from "react-query";
 
 const defaultAnimationOptions = {
@@ -17,33 +27,34 @@ const defaultAnimationOptions = {
   autoplay: true,
   animationData: notFoundAnimation,
   rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 
 const downloadImage = (id) => {
   const input = document.getElementById("tedxsjec-ticket");
-  toPng(input).then(function (dataUrl) {
-    var img = new Image();
-    img.src = dataUrl;
-    document.body.appendChild(img);
-    var link = document.createElement('a');
-    link.download = `TEDxSJEC-2022-Ticket-${id}`;
-    link.href = img.src;
-    link.click();
-    
-    // Code for PDF download
-    // const pdf = new jsPDF();
-    // var width = pdf.internal.pageSize.getWidth();
-    // var height = pdf.internal.pageSize.getHeight();
-    // pdf.addImage(img.src, "PNG", 0, 0, width, height);
-    // pdf.save(`TEDxSJEC-2022-Ticket-${params.id}.pdf`);
+  toPng(input)
+    .then(function (dataUrl) {
+      var img = new Image();
+      img.src = dataUrl;
+      document.body.appendChild(img);
+      var link = document.createElement("a");
+      link.download = `TEDxSJEC-2022-Ticket-${id}`;
+      link.href = img.src;
+      link.click();
 
-    document.body.removeChild(img)
-  })
-  .catch(function (error) {
-    console.error('Oops, something went wrong!', error);
-  });
+      // Code for PDF download
+      // const pdf = new jsPDF();
+      // var width = pdf.internal.pageSize.getWidth();
+      // var height = pdf.internal.pageSize.getHeight();
+      // pdf.addImage(img.src, "PNG", 0, 0, width, height);
+      // pdf.save(`TEDxSJEC-2022-Ticket-${params.id}.pdf`);
+
+      document.body.removeChild(img);
+    })
+    .catch(function (error) {
+      console.error("Oops, something went wrong!", error);
+    });
 };
 
 const Ticket = () => {
@@ -51,30 +62,35 @@ const Ticket = () => {
   let params = useParams();
   document.title = `Ticket - ${params.id}  | TEDxSJEC 2022`;
 
-  const { data: ticketData, isLoading, isError } = useQuery(params.id, () => {
-    return axios.post(`${process.env.REACT_APP_SERVER_URL}/api/ticket?id=${params.id}`)
-  })
-  
+  const {
+    data: ticketData,
+    isLoading,
+    isError,
+  } = useQuery(params.id, () => {
+    return axios.post(
+      `${process.env.REACT_APP_SERVER_URL}/api/ticket?id=${params.id}`
+    );
+  });
+
   useEffect(() => {
-    if(ticketData) {
-      setTicket(ticketData.data)
+    if (ticketData) {
+      setTicket(ticketData.data);
     }
   }, [ticketData]);
 
   return (
     <LoadingOverlay active={isLoading} spinner text="Loading.....">
       <ParentWrapper>
-        { isLoading ? <Logo /> : 
-        (ticket && !isError ? (
+        {isLoading ? (
+          <Logo />
+        ) : ticket && !isError ? (
           <>
             <ChildWrapper id="tedxsjec-ticket">
               <Logo />
-              <div style={{ display: 'flex' }}>
+              <div style={{ display: "flex" }}>
                 <TicketSection>
                   <TicketHeading>NAME</TicketHeading>
-                  <TicketText>
-                    {ticket ? ticket.name : "Loading..."}
-                  </TicketText>
+                  <TicketText>{ticket ? ticket.name : "Loading..."}</TicketText>
                 </TicketSection>
                 <TicketSection>
                   <TicketHeading>PHONE</TicketHeading>
@@ -91,22 +107,24 @@ const Ticket = () => {
                   </TicketText>
                 </TicketSection>
               </div>
-              <div style={{ display: 'flex' }}>
+              <div style={{ display: "flex" }}>
                 <TicketSection>
                   <TicketHeading>DATE</TicketHeading>
-                  <TicketText>
-                    June 11, 2022
-                  </TicketText>
+                  <TicketText>June 11, 2022</TicketText>
                 </TicketSection>
                 <TicketSection>
                   <TicketHeading>TIME</TicketHeading>
-                  <TicketText>
-                    9:00 AM
-                  </TicketText>
+                  <TicketText>9:00 AM</TicketText>
                 </TicketSection>
               </div>
               <QRCodeContainer>
-                <QRCode style={{ boxShadow: '0 0 10px #444' }} bgColor="#FFFFFF" fgColor="#000000" value={params.id} size={200} />
+                <QRCode
+                  style={{ boxShadow: "0 0 10px #444" }}
+                  bgColor="#FFFFFF"
+                  fgColor="#000000"
+                  value={params.id}
+                  size={200}
+                />
               </QRCodeContainer>
               <OrderIdContainer>
                 <OrderId>
@@ -115,32 +133,36 @@ const Ticket = () => {
               </OrderIdContainer>
             </ChildWrapper>
             <div>
-              <button style={{ marginTop: '400px' }}
-                  onClick={() => downloadImage(params.id)}
-                  className="btn btn-tedx mt-3"
-                >
+              <button
+                style={{ marginTop: "400px" }}
+                onClick={() => downloadImage(params.id)}
+                className="btn btn-tedx mt-3"
+              >
                 Download Ticket
               </button>
             </div>
-        </>)
-       : (
-        <>
-          <Logo />
-          <Lottie 
-            options={defaultAnimationOptions}
-            height={300}
-            width={300}
-          />
-          <ErrorText>Unfortunately, we could not find your ticket!</ErrorText>
-          {/* TODO: Make button go back to home page / registration page */}
-          <div>
-            <button className="btn btn-tedx mt-3">Go back</button>
-          </div>
-        </>
-        )        
-      )}
+          </>
+        ) : (
+          <>
+            <Logo />
+            <Lottie
+              options={defaultAnimationOptions}
+              height={300}
+              width={300}
+            />
+            <ErrorText>Unfortunately, we could not find your ticket!</ErrorText>
+            <div>
+              <button
+                onClick={() => (window.location.href = "/")}
+                className="btn btn-tedx mt-3"
+              >
+                Back!
+              </button>
+            </div>
+          </>
+        )}
       </ParentWrapper>
-    </LoadingOverlay>  
+    </LoadingOverlay>
   );
 };
 
