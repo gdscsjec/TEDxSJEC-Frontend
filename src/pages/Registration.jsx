15e99bf4632d15519e06e220d28d6295e479aaa3
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2022-05-19 07:30:29 +0530

    Deployment Fix

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -6,8 +6,6 @@ import { ParentWrapper, ChildWrapper } from "../styles/layout.style";
 import LoadingOverlay from "react-loading-overlay";
 import { useContext } from "react";
 import loadingContext from "../context/loadingContext";
-
-// Fix for package 'react-loading-overlay' since the repo is inactive.
 LoadingOverlay.propTypes = undefined;
 
 const Registration = () => {
