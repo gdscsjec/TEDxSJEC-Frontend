46f1f7e9814a53c105f30dfbb4837cddab06a0b1
vigneshshettyin <vigneshshetty.in@gmail.com>
2022-05-31 20:36:58 +0530

    Service Down

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import Registration from "./pages/Registration";
 import Ticket from "./pages/Ticket";
 import { QueryClientProvider, QueryClient } from "react-query";
-import RefundPolicy from './pages/RefundPolicy';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import Terms from './pages/Terms';
-
+import RefundPolicy from "./pages/RefundPolicy";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Terms from "./pages/Terms";
+import Down from "./pages/Down";
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Routes>
-        <Route path="/" element={<Registration />} />
+        <Route path="/" element={<Down />} />
+        <Route
+          path="/63c83774-3009-49e2-a6e9-87b8ca9c3ffa/live"
+          element={<Registration />}
+        />
         <Route path="/ticket/:id" element={<Ticket />} />
         <Route path="/refund-policy" element={<RefundPolicy />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
---
+++ src/pages/Down.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Down = () => {
+  return <h1>Service will be back online on 01-06-2022 6:00 AM.</h1>;
+};
+
+export default Down;
