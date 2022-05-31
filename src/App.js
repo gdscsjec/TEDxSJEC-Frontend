365287de75f5ceb218be1289695d43c085bbeba3
vigneshshettyin <vigneshshetty.in@gmail.com>
2022-06-01 01:06:41 +0530

    Taking-Site-Live

--- src/App.js
+++ src/App.js
@@ -6,23 +6,19 @@ import { QueryClientProvider, QueryClient } from "react-query";
 import RefundPolicy from "./pages/RefundPolicy";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import Terms from "./pages/Terms";
-import Down from "./pages/Down";
+// import Down from "./pages/Down";
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Routes>
-        <Route path="/" element={<Down />} />
-        <Route
-          path="/8dffda0b-e9a6-4afb-bd59-873b7907807a/live"
-          element={<Registration />}
-        />
+        <Route path="/" element={<Registration />} />
         <Route path="/ticket/:id" element={<Ticket />} />
         <Route path="/refund-policy" element={<RefundPolicy />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms-and-conditions" element={<Terms />} />
-        <Route path="*" element={<Down />} />
+        <Route path="*" element={<Registration />} />
       </Routes>
     </QueryClientProvider>
   );
